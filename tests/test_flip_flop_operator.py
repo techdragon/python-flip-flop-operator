c7a0011c9ce0ddf3294134ac9fc087bec98b093f
Samuel Bishop <sam@psyx.co>
2018-08-28 12:26:15 +1000

    Tests!

--- tests/test_flip_flop_operator.py
+++ tests/test_flip_flop_operator.py
@@ -1,2 +1,10 @@
-def test_flip_flop():
-    pass
+from flip_flop_operator import flip_flop
+
+
+def test_flip_flop_basic_functionality():
+    for i in range(25):
+        if (i == 4) | flip_flop | (i == 10):
+            assert 5 <= i <= 9
+        if (i == 12) | flip_flop | (i == 15):
+            assert 13 <= i <= 14
+
